eb2463bb64a6bee3b5e5c3b11744bfbc96586420
charlesduquesnoy <charles.duquesnoy@ipsa.fr>
2022-12-10 09:54:14 +0100

    Download phot proto

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
 
 import numpy as np 
+from sentinelsat import SentinelAPI
+import traceback
+
+user = 'hugsy'
+password = '9cCxYTDh'
+api = SentinelAPI(user, password, 'https://scihub.copernicus.eu/dhus')
 
 
-print("First Git Project")
+with open('uuid.txt') as f:
+    lines = f.readlines()
+
+compteur  = 0
+
+for i in range(1,len(lines)):
+    try :
+        print(lines[i][0:-1])
+        request = lines[i][0:-1]
+        api.download(str(request))
+    except :
+        traceback.print_exc()
+        compteur = compteur +  1
+        print('compteur = ',compteur)
+        continue
+print('Finish')
+
